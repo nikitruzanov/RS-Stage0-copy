84ccdf25e82d980dd8aae8d4a9869a327a7de1b9
Nikita Ruzanov <nikitruzanov@gmail.com>
2024-08-22 03:17:13 +0400

    feat: update for shelter-3

---
+++ shelter/assets/js/popapp.js
@@ -0,0 +1,37 @@
+let petsData = [];
+
+fetch('data/pets.json')
+    .then(res => res.json())
+    .then(data => {
+        petsData = data;
+        console.log(petsData);
+    })
+
+
+
+document.querySelectorAll('.pets-item').forEach(item => {
+item.addEventListener('click', function () {
+
+    const index = this.getAttribute('data-index'); // Получаем индекс из атрибута data-index
+
+    // Заполняем попап данными из JSON
+    const pet = petsData[index];
+    document.querySelector('#popup-img').src = pet.img;
+    document.querySelector('#popup-name').textContent = pet.name;
+    document.querySelector('#popup-breed').textContent = `${pet.type} - ${pet.breed}`;
+    document.querySelector('#popup-description').textContent = pet.description;
+    document.querySelector('#popup-age').textContent = pet.age;
+    document.querySelector('#popup-inoculations').textContent = pet.inoculations;
+    document.querySelector('#popup-diseases').textContent = pet.diseases;
+    document.querySelector('#popup-parasites').textContent = pet.parasites;
+
+    document.querySelector('#popup-overlay').classList.toggle('active');
+    document.body.classList.toggle('block');
+});
+});
+
+document.querySelector('.close-btn, #popup-overlay').addEventListener('click', function () {
+        document.querySelector('#popup-overlay').classList.remove('active');
+        document.body.classList.remove('block');
+    });
+
