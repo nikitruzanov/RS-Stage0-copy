780641a784e0c83afbc1bbf5d226e7d942be22d0
Nikita Ruzanov <nikitruzanov@gmail.com>
2024-09-22 18:09:00 +0400

    feat: update code

--- audio-player/script.js
+++ audio-player/script.js
@@ -5,30 +5,35 @@ const data = {
             "audio": "assets/audio/Luverance - Душа моя.mp3",
             "author": "Luverance",
             "name": "Душа моя",
+            "duration": "02:55",
         },
         {
             "image": "assets/img/two.jpg",
             "audio": "assets/audio/The Hatters, TRITIA - Где-то там.mp3",
             "author": "TRITIA",
             "name": "Где-то там",
+            "duration": "02:58",
         },
         {
             "image": "assets/img/three.jpg",
             "audio": "assets/audio/Порнофильмы - Я так соскучился.mp3",
             "author": "Порнофильмы",
             "name": "Я так соскучился",
+            "duration": "06:49",
         },
         {
             "image": "assets/img/four.jpg",
             "audio": "assets/audio/Luverance Там где.mp3",
             "author": "Luverance",
             "name": "Там где",
+            "duration": "03:17",
         },
         {
             "image": "assets/img/five.jpg",
             "audio": "assets/audio/TRITIA-Города.mp3",
             "author": "TRITIA",
             "name": "Города",
+            "duration": "02:49",
         },
     ]
 }
