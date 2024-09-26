6724339dacfa255d6c6054644654fd053b9d4526
Nikita Ruzanov <nikitruzanov@gmail.com>
2024-09-27 01:34:25 +0400

    feat: add score and fix

--- random-game/script.js
+++ random-game/script.js
@@ -1,7 +1,17 @@
 const character = document.getElementById("character");
 const barrier = document.getElementById("barrier");
+const scoreElement = document.getElementById("score");
 let game;
-let isGameOver = false;
+let score = 0;
+
+let isAlive = setInterval(function () {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let barrierLeft = parseInt(window.getComputedStyle(barrier).getPropertyValue("left"));
+
+    if (barrierLeft < 50 && barrierLeft > 0 && characterTop >= 140 ) {
+        endGame();
+    }
+}, 10)
 
 document.addEventListener("keydown", function (event) {
     jump();
@@ -16,11 +26,17 @@ function jump() {
     }, 300)
 }
 
-let isAlive = setInterval(function () {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let barrierLeft = parseInt(window.getComputedStyle(barrier).getPropertyValue("left"));
-    
-    if (barrierLeft < 50 && barrierLeft > 0 && characterTop >= 140 ) {
-        endGame();
-    }
-}, 10)
+function startGame() {
+    game = setInterval(isAlive, 10);
+    barrier.style.animation = '';
+}
+
+function endGame() {
+    clearInterval(game);
+    barrier.style.animation = 'none';
+    alert("Game Over! Нажмите кнопку для перезапуска игры.");
+}
+
+document.getElementById('restartButton').addEventListener('click', function () {
+    startGame();
+});
