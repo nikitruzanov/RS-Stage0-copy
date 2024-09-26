26a0bd3771599d3a71fcb5ec811c1a26b4cd42a2
Nikita Ruzanov <nikitruzanov@gmail.com>
2024-09-27 02:56:51 +0400

    feat: update for setting

--- random-game/script.js
+++ random-game/script.js
@@ -3,15 +3,40 @@ const barrier = document.getElementById("barrier");
 const scoreElement = document.getElementById("score");
 let game;
 let score = 0;
+let isGameRunning = false;
+let barrierSpeed = 3;
+
+document.querySelectorAll(".button").forEach((button) => {
+    button.addEventListener("click", function () {
+        document
+            .querySelectorAll(".button")
+            .forEach((btn) => btn.classList.remove("active"));
+
+        button.classList.add("active");
+    });
+});
+
+function updateScore() {
+    scoreElement.textContent = `Score: ${score}`;
+}
 
 let isAlive = setInterval(function () {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let barrierLeft = parseInt(window.getComputedStyle(barrier).getPropertyValue("left"));
+    if (!isGameRunning) return;
 
-    if (barrierLeft < 50 && barrierLeft > 0 && characterTop >= 140 ) {
+    let characterTop = parseInt(
+        window.getComputedStyle(character).getPropertyValue("top"),
+    );
+    let barrierLeft = parseInt(
+        window.getComputedStyle(barrier).getPropertyValue("left"),
+    );
+
+    if (barrierLeft < 50 && barrierLeft > 0 && characterTop >= 140) {
         endGame();
+    } else if (barrierLeft < -50) {
+        score++;
+        updateScore();
     }
-}, 10)
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
@@ -23,20 +48,56 @@ function jump() {
     }
     setTimeout(function () {
         character.classList.remove("jump");
-    }, 300)
+    }, 300);
 }
 
-function startGame() {
-    game = setInterval(isAlive, 10);
-    barrier.style.animation = '';
+function toggleButtonsState(disabled) {
+    document.querySelectorAll('.button').forEach(button => {
+        button.disabled = disabled;
+    });
 }
 
-function endGame() {
-    clearInterval(game);
-    barrier.style.animation = 'none';
-    alert("Game Over! Нажмите кнопку для перезапуска игры.");
-}
+    function startGame() {
+        game = setInterval(isAlive, 10);
+        score = 0;
+        updateScore();
+        isGameRunning = true;
+        setBarrierSpeed(barrierSpeed);
+        barrier.style.animation = "";
 
-document.getElementById('restartButton').addEventListener('click', function () {
-    startGame();
-});
+        if (isGameRunning) {
+            toggleButtonsState(true);
+        }
+    }
+
+    function endGame() {
+        clearInterval(game);
+        isGameRunning = false;
+        barrier.style.animation = "none";
+        toggleButtonsState(false);
+        alert(` 
+         Ваш рекорд: ${score}
+         Нажмите кнопку для перезапуска игры.`);
+    }
+
+    document.getElementById("restartButton").addEventListener("click", function () {
+        startGame();
+    });
+
+    function setBarrierSpeed(speed) {
+        barrier.classList.remove('speed1', 'speed2', 'speed3');
+        barrier.classList.add(`speed${speed}`);    }
+
+    document.getElementById("easy").addEventListener("click", function () {
+        barrierSpeed = 3;
+    });
+
+    document.getElementById("medium").addEventListener("click", function () {
+        barrierSpeed = 2;
+    });
+
+    document.getElementById("hard").addEventListener("click", function () {
+        barrierSpeed = 1;
+    });
+
+startGame();
